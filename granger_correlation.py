d5ea13c2f0929fd3cae10f5566663ad898425690
albornet <alban.bornet@epfl.ch>
2020-01-08 09:37:07 +0100

    update download link

--- granger_correlation.py
+++ granger_correlation.py
@@ -21,8 +21,10 @@ def build_one_part_of_the_full_matrix(start_index):
         scholar_time_series_local = scholar_time_series[           :n_scientists]
 
     # Build the granger causality matrix
-    pearson = numpy.zeros((len(twitter_time_series_local), len(scholar_time_series_local)))
-    granger = numpy.zeros((len(twitter_time_series_local), len(scholar_time_series_local)))
+    pearson_05 = numpy.zeros((len(twitter_time_series_local), len(scholar_time_series_local)))
+    pearson_01 = numpy.zeros((len(twitter_time_series_local), len(scholar_time_series_local)))
+    granger_05 = numpy.zeros((len(twitter_time_series_local), len(scholar_time_series_local)))
+    granger_01 = numpy.zeros((len(twitter_time_series_local), len(scholar_time_series_local)))
     for t, twitter_time in enumerate(twitter_time_series_local):
         print('Processing scientist number %4i...' % (start_index+t))
         for s, scholar_time in enumerate(scholar_time_series_local):
@@ -32,16 +34,22 @@ def build_one_part_of_the_full_matrix(start_index):
                 r, p_value_r = pearsonr(twitter_time, scholar_time)
                 g_results    = grangercausalitytests(time_array, max_lag, verbose=False)
             p_values_g     = [g_results[i+1][0][g_test][1] for i in range(max_lag)]
-            g_values_g     = [g_results[i+1][0][g_test][1] for i in range(max_lag)]
+            g_values_g     = [g_results[i+1][0][g_test][0] for i in range(max_lag)]
             p_value_g, idx = min((val, idx) for (idx, val) in enumerate(p_values_g))
             if p_value_r < 0.05:
-                pearson[t,s] = r
+                pearson_05[t,s] = r
+            if p_value_r < 0.01:
+                pearson_01[t,s] = r
             if p_value_g < 0.05:
-                granger[t,s] = g_values_g[idx]
+                granger_05[t,s] = g_values_g[idx]
+            if p_value_g < 0.01:
+                granger_01[t,s] = g_values_g[idx]
 
     # Save the matrices to build them afterwards
-    numpy.save('pearson_' + str(start_index) + '.npy', pearson)
-    numpy.save('granger_' + str(start_index) + '.npy', granger)
+    numpy.save('pearson_05_' + str(start_index) + '.npy', pearson_05)
+    numpy.save('pearson_01_' + str(start_index) + '.npy', pearson_01)
+    numpy.save('granger_05_' + str(start_index) + '.npy', granger_05)
+    numpy.save('granger_01_' + str(start_index) + '.npy', granger_01)
 
 
 # Main code
@@ -54,7 +62,7 @@ if __name__ == '__main__':
     # Initialize the time-series from the csv (tsv) datasets
     twitter_time_series = []
     scholar_time_series = []
-    for i, idx in enumerate(twitter_data['id']):
+    for idx in twitter_data['id']:
         scholar_t = numpy.array(scholar_data.loc[scholar_data['id'] == idx])[0,-14:]
         if sum(scholar_t) > 0:
             twitter_t = numpy.array(twitter_data.loc[twitter_data['id'] == idx])[0,-14:]
@@ -64,7 +72,7 @@ if __name__ == '__main__':
     # Initialize some numbers for the statistical tests
     n_time_points = len(twitter_time_series[0])
     max_lag       = n_time_points//3
-    g_test        = 'ssr_chi2test'
+    g_test        = 'ssr_ftest'
 
     # Initialize some numbers for the multi-threading
     n_cores_to_use = 7  # define here how many cores you want to use
@@ -84,18 +92,29 @@ if __name__ == '__main__':
             build_one_part_of_the_full_matrix(start_index)
 
     # Re-build the complete matrices
-    pearson = numpy.concatenate([numpy.load('pearson_' + str(s) + '.npy') for s in start_indexes], axis=0)
-    granger = numpy.concatenate([numpy.load('granger_' + str(s) + '.npy') for s in start_indexes], axis=0)
-    numpy.save('pearson', pearson)
-    numpy.save('granger', granger)
+    pearson_05 = numpy.concatenate([numpy.load('pearson_05_' + str(s) + '.npy') for s in start_indexes], axis=0)
+    pearson_01 = numpy.concatenate([numpy.load('pearson_01_' + str(s) + '.npy') for s in start_indexes], axis=0)
+    granger_05 = numpy.concatenate([numpy.load('granger_05_' + str(s) + '.npy') for s in start_indexes], axis=0)
+    granger_01 = numpy.concatenate([numpy.load('granger_01_' + str(s) + '.npy') for s in start_indexes], axis=0)
+    numpy.save('conjoint_data/pearson_05.npy', pearson_05)
+    numpy.save('conjoint_data/pearson_01.npy', pearson_01)
+    numpy.save('conjoint_data/granger_05.npy', granger_05)
+    numpy.save('conjoint_data/granger_01.npy', granger_01)
 
     # Delete the partial matrices
     for start_index in start_indexes:
-        os.remove('pearson_' + str(start_index) + '.npy')
+        os.remove('pearson_05_' + str(start_index) + '.npy')
+        os.remove('pearson_01_' + str(start_index) + '.npy')
+        os.remove('granger_05_' + str(start_index) + '.npy')
+        os.remove('granger_01_' + str(start_index) + '.npy')
 
     # Plot the thing
     plt.figure(0)
-    plt.imshow(pearson)
+    plt.imshow(pearson_05)
     plt.figure(1)
-    plt.imshow(granger)
+    plt.imshow(pearson_01)
+    plt.figure(2)
+    plt.imshow(granger_05)
+    plt.figure(3)
+    plt.imshow(granger_01)
     plt.show()
---
+++ graph_filtering.py
@@ -0,0 +1,74 @@
+import pandas
+import numpy
+import html
+import csv
+import os
+import matplotlib.pyplot as plt
+rebuild_from_raw = 0
+
+# Build the matrix from raw data
+if rebuild_from_raw:
+
+    # Deal with html escaped characters and errors during the data collection in the coauthors file
+    with open('scholar_data/coauthors.tsv', 'r') as f, open('scholar_data/coauthors_2.tsv', 'w') as g:
+        g.write(''.join(html.unescape(f.read()).split(' <span class=""homonym-nr')))
+    os.remove('scholar_data/coauthors.tsv')
+    os.rename('scholar_data/coauthors_2.tsv', 'scholar_data/coauthors.tsv')
+
+    # Initialize scholar and twitter processed time-series
+    twitter_data = pandas.read_csv('twitter_data/twitter_stats_month.csv',         sep=',' ).sort_values(by='id')
+    scholar_data = pandas.read_csv('scholar_data/citations.tsv',                   sep='\t').sort_values(by='id')
+    coauths_list = pandas.read_csv('scholar_data/coauthors.tsv', low_memory=False, sep='\t').sort_values(by='id')
+
+    # Remove the people that are not in the twitter_data list (no active twitter account anymore)
+    twitter_ids  = list(twitter_data['id'])
+    drop_indexes = []
+    for i, idx in enumerate(scholar_data['id']):
+        if idx not in twitter_ids:
+            drop_indexes.append(i)
+    scholar_data = scholar_data.drop(drop_indexes)
+    coauths_list = coauths_list.drop(drop_indexes)
+
+    # Remove the scientists that have no publications, because we don't know if they really have
+    twitter_data = twitter_data[list(scholar_data.loc[:,'2006':'2019'].sum(axis=1) > 0)].reset_index(drop=True)
+    coauths_list = coauths_list[list(scholar_data.loc[:,'2006':'2019'].sum(axis=1) > 0)].reset_index(drop=True)
+    scholar_data = scholar_data[list(scholar_data.loc[:,'2006':'2019'].sum(axis=1) > 0)].reset_index(drop=True)
+
+    # Build the graph from coauthors
+    scientist_names = list(scholar_data['name'])
+    n_scientists    = len(scholar_data)
+    A_coauthors     = numpy.zeros((n_scientists, n_scientists), dtype=int)
+    print('Building the coauthors adjajency matrix:')
+    for i, row in coauths_list.iterrows():
+        if i%1000 == 0:
+            print('\t%4i scientists over %4i done...' % (i, n_scientists))
+        for coauthor_name in row['1':'1021'].dropna():
+            if coauthor_name in scientist_names:
+                A_coauthors[i, scientist_names.index(coauthor_name)] = 1
+
+    # Save the raw matrix
+    numpy.save('A_coauthors_raw', A_coauthors)
+
+# Load the raw matrix
+else:
+    A_coauthors = numpy.load('A_coauthors_raw.npy')
+
+# Make the adjacency matrix symmetric (because it has to) save it
+n_non_symmetric_values = numpy.count_nonzero(A_coauthors - A_coauthors.transpose())
+if n_non_symmetric_values > 0:
+    percent_non_symmetric = 100.0*n_non_symmetric_values/A_coauthors.sum()
+    print('Completing the coauthor matrix (%2i%% non symmetric values).' % (percent_non_symmetric))
+    A_coauthors = (A_coauthors + A_coauthors.transpose()).clip(max=1)
+numpy.save('A_coauthors', A_coauthors)
+
+# Build the degree distribution
+D_coauthors = A_coauthors.sum(axis=0)
+hist_axis   = numpy.ones(D_coauthors.shape[0])/D_coauthors.shape[0]
+
+# Plot everything
+fig, axes = plt.subplots(1, 2, figsize=(16, 4))
+axes[0].set_title('Coauthors graph adjacency matrix')
+axes[0].imshow(A_coauthors)
+axes[1].set_title('Coauthor graph degree distribution')
+axes[1].hist(D_coauthors, weights=hist_axis)
+plt.show()
